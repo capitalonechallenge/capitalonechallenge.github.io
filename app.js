cff5a204c82f03bbaae5410bee7fcecf4962556d
Akshay Jain <akshay.j.8899@gmail.com>
2019-03-11 14:38:32 -0400

    new

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ if(sPage == "search.html"){
 }
 
 var nexturl;
+var previousurl;
 
 function search2() {
 	var url_string = window.location.href
@@ -21,19 +22,34 @@ function search2() {
 	var result = httpGetAsync(query, parse);
 }
 
+document.getElementById('previous').style.display = "none";
 
 function parse(result){
 	var parsed = JSON.parse(result);
 	var total = parsed.collection.metadata.total_hits;
+	//document.getElementById('numbers').innerHTML = result;
 	
 	var url_string = window.location.href
 	var url = new URL(url_string);
 	var search_val = url.searchParams.get("q");
-	document.getElementById('numbers').innerHTML = "1-100 of " + String(total) + " for \"" + search_val + "\":";
+	var page_num = url.searchParams.get("page");
+	document.getElementById('numbers').innerHTML = String(1+(page_num-1)*100)+"-"+String(page_num*100) + " of " + String(total) + " for \"" + search_val + "\":";
 	var pictures = parsed.collection.items;
 	display_images(pictures);
 	
-	var next_url = parsed.collection.links[0].href;
+	var size = Object.keys(parsed.collection.links).length;
+	if(size == 2){
+		var next_url = parsed.collection.links[1].href;
+		var previous_url = parsed.collection.links[0].href;
+		
+		var params = previous_url.split("?")[1];
+		previousurl = "search.html?" + String(params);
+		
+		document.getElementById('previous').style.display = "block";		
+	}
+	if(size == 1){
+		var next_url = parsed.collection.links[0].href;
+	}
 	var params = next_url.split("?")[1];
 	nexturl = "search.html?" + String(params);
 	//alert(String(params));
@@ -41,6 +57,10 @@ function parse(result){
 
 function next(){
 	window.location.href = nexturl;
+}
+
+function previous(){
+	window.location.href = previousurl;
 }
 
 function img_create(src) {
